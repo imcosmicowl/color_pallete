fd3329a3095a7b75e85d19cfa61d0bb83d154289
imcosmicwol <marcos.melgar.segovia@gmail.com>
2021-11-25 22:43:21 +0100

    removed unused code

--- main.py
+++ main.py
@@ -26,104 +26,6 @@ args = parser.parse_args()
 
 # GLOBALS DEFFINITIONS
 
-
-def get_color_pallete_(input_file, output_file, num_colors, display_color=False):
-    img = plt.imread(input_file)
-
-    red, green, blue = [], [], []
-    for line in img:
-        for pixel in line:
-            r, g, b = pixel
-            red.append(r)
-            green.append(g)
-            blue.append(b)
-
-    df = pd.DataFrame({
-        'red': red,
-        'green': green,
-        'blue': blue
-    })
-
-    df['standardized_red'] = cluster.vq.whiten(df['red'])
-    df['standardized_green'] = cluster.vq.whiten(df['green'])
-    df['standardized_blue'] = cluster.vq.whiten(df['blue'])
-
-    color_pallete, distortion = cluster.vq.kmeans(df[['standardized_red', 'standardized_green', 'standardized_blue']],
-                                                  num_colors)
-    colors = []
-    red_std, green_std, blue_std = df[['red', 'green', 'blue']].std()
-    for color in color_pallete:
-        scaled_red, scaled_green, scaled_blue = color
-        colors.append((
-            math.ceil(scaled_red * red_std),
-            math.ceil(scaled_green * green_std),
-            math.ceil(scaled_blue * blue_std)
-        ))
-
-    colors.sort(key=lambda x: step(x[0], x[1], x[2], 8))
-
-    # FIXME: need a smart way to resize fonts based on picture size
-    font_size = 11
-    font = ImageFont.truetype("Roboto-Medium.ttf", font_size)
-    sample_text = '#F8F8F7'
-    proper_font_size = False
-
-    pil_img = Image.open(input_file)
-    pil_width, pil_height = pil_img.size
-    height = 0
-    if pil_height > pil_width:
-        height = math.floor(pil_height / 6)
-    else:
-        height = math.floor(pil_height / 4)
-
-    pallete = Image.new('RGB', (pil_width, height), (255, 255, 255))
-    single_img_space = math.floor(pil_width / num_colors)
-    single_img_offset = math.floor(single_img_space / 14)
-    total_offset = single_img_offset * (num_colors + 1)
-    single_img_width = math.floor((pil_width - total_offset) / num_colors)
-    single_img_space = single_img_width + single_img_offset
-
-    final_img_width = (single_img_width + (pil_width - (single_img_space * num_colors))) - single_img_offset
-
-    while not proper_font_size:
-        if get_text_width(font, sample_text) > single_img_width and font_size > 1:
-            font_size -= 1
-            font = ImageFont.truetype("Roboto-Medium.ttf", font_size)
-        elif get_text_width(font, sample_text) < single_img_width - 20:
-            font_size += 1
-            font = ImageFont.truetype("Roboto-Medium.ttf", font_size)
-        else:
-            proper_font_size = True
-
-    x_offset = 0
-    for i in range(len(colors)):
-        if i == len(colors) - 1:
-            new_img = Image.new('RGB', (final_img_width, height), colors[i])
-            pallete.paste(new_img, (x_offset, 0))
-            if display_color:
-                draw = ImageDraw.Draw(pallete)
-                draw.text((x_offset, height - 20 - get_text_height(font, sample_text)), get_hex_color(colors[i]),
-                          (255, 255, 255), font=font)
-        elif i == 0:
-            new_img = Image.new('RGB', (single_img_width, height), colors[i])
-            pallete.paste(new_img, (single_img_offset, 0))
-            if display_color:
-                draw = ImageDraw.Draw(pallete)
-                draw.text((single_img_offset, height - 20 - get_text_height(font, sample_text)),
-                          get_hex_color(colors[i]), (255, 255, 255), font=font)
-            x_offset += single_img_space + single_img_offset
-        else:
-            new_img = Image.new('RGB', (single_img_width, height), colors[i])
-            pallete.paste(new_img, (x_offset, 0))
-            if display_color:
-                draw = ImageDraw.Draw(pallete)
-                draw.text((x_offset, height - 20 - get_text_height(font, sample_text)), get_hex_color(colors[i]),
-                          (255, 255, 255), font=font)
-            x_offset += single_img_space
-
-    pallete.save(output_file)
-
-
 def get_color_pallete(input_file, num_colors):
     color_thief = ColorThief(input_file)
     palette = color_thief.get_palette(color_count=num_colors)
@@ -191,38 +93,8 @@ def create_pallete(filename, num_colors, display_color=False):
     append_color_pallete(filename, output_palette, output_combined)
 
 
-def step(r, g, b, repititions=1):
-    lum = math.sqrt(0.241 * r + 0.691 * g + 0.068 * b)
-
-    h, s, v = colorsys.rgb_to_hsv(r, g, b)
-
-    h2 = int(h * repititions)
-    lum2 = int(lum * repititions)
-    v2 = int(v * repititions)
-
-    if h2 % 2 == 1:
-        v2 = repititions - v2
-        lum = repititions - lum
-
-    return (h2, lum, v2)
-
-
 def get_hex_color(color):
     return '#%02x%02x%02x' % color
-
-
-def get_text_width(font, text):
-    width = 0
-    for ch in text:
-        width += font.getsize(ch)[0]
-    return widthlum2
-
-
-def get_text_height(font, text):
-    height = []
-    for ch in text:
-        height.append(font.getsize(ch)[1])
-    return max(height)
 
 
 def main(path, num_colors, text):
